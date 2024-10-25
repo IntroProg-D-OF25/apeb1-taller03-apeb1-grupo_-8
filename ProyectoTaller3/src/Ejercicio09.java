380179dba63668d59f850b92cbf3f6043a16adca
evelynlizzethe <elriofrio1@utpl.edu.ec>
2024-10-24 22:28:28 -0500

    Ejercicios

--- ProyectoTaller3/src/Ejercicio09.java
+++ ProyectoTaller3/src/Ejercicio09.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author USUARIO
- */
 public class Ejercicio09 {
-    
+    public static void main(String[] args) {
+        int v1, v2, v3, v4; 
+        boolean respuesta; 
+        v1 = 81;
+        v2 = 25; 
+        v3 = 10; 
+        v4 = 50; 
+        respuesta = ((Math.sqrt(v1) + (Math.sqrt(v2) * (v3)) ) >= 50);
+        System.out.println("respuesta = " + respuesta);
+    }
 }
